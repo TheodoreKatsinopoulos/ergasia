4bdf47510a4e893d72c07338235ef039e24fad4b
theodore <tedk95@live.com>
2021-02-23 19:59:49 -0500

    route.rb fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,7 @@
 # config/routes.rb
 Rails.application.routes.draw do
-<<<<<<< HEAD
-
-  scope module: :v2, constraints: ApiVersion.new('v2') do
-    resources :todos, only: :index
-  end
-
-  scope module: :v1, constraints: ApiVersion.new('v1', true) do
-   resources :todos do
-     resources :items
-   end
-   
-=======
   resources :todos do
     resources :items
->>>>>>> parent of e4ab49b... VErsioning v1
   end
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
